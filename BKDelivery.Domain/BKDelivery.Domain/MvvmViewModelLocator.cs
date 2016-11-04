80d57e5c51d22f929300656a5f5e89eda802c831
268737 (Michał Baryga) <268737@pw.edu.pl>
2016-11-04 19:06:09 +0100

    Hey

---
+++ BKDelivery.Domain/BKDelivery.Domain/MvvmViewModelLocator.cs
@@ -0,0 +1,54 @@
+﻿/*
+  In App.xaml:
+  <Application.Resources>
+      <vm:MvvmViewModelLocator xmlns:vm="clr-namespace:BKDelivery.Domain"
+                                   x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+*/
+
+
+namespace BKDelivery.Domain
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MvvmViewModelLocator
+    {
+        static MvvmViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                // SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
+            }
+            else
+            {
+                // SimpleIoc.Default.Register<IDataService, DataService>();
+            }
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        /// <summary>
+        /// Gets the Main property.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+    }
+}
